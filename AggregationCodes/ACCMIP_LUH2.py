15e8686b74cf80dbb40ec8c659baedca2bd6fcd4
Srishti Vishwakarma <40209656+svish91@users.noreply.github.com>
2023-02-09 00:35:47 -0500

    Update ACCMIP_LUH2.py

--- AggregationCodes/ACCMIP_LUH2.py
+++ AggregationCodes/ACCMIP_LUH2.py
@@ -9,16 +9,16 @@ import geopandas as gp
 
 shapefile = gp.read_file(r"99bfd9e7-bb42-4728-87b5-07f8c8ac631c2020328-1-1vef4ev.lu5nk.shp")
 # columns: OBJECTID	CNTRY_NAME
-df_NOx_kgNha = pd.DataFrame(0, index=np.arange(len(shapefile)), columns=np.arange(1961,2019,1))
+df_NOx_kgNha = pd.DataFrame(0, index=np.arange(len(shapefile)), columns=np.arange(1961,2020,1))
 df_NOx_kgNha = df_NOx_kgNha.rename(index = shapefile['CNTRY_NAME'])
-df_NHy_kgNha= pd.DataFrame(0, index=np.arange(len(shapefile)), columns=np.arange(1961,2019,1))
+df_NHy_kgNha= pd.DataFrame(0, index=np.arange(len(shapefile)), columns=np.arange(1961,2020,1))
 df_NHy_kgNha = df_NHy_kgNha.rename(index = shapefile['CNTRY_NAME'])
-df_totNdep_kgNha= pd.DataFrame(0, index=np.arange(len(shapefile)), columns=np.arange(1961,2019,1))
+df_totNdep_kgNha= pd.DataFrame(0, index=np.arange(len(shapefile)), columns=np.arange(1961,2020,1))
 df_totNdep_kgNha = df_totNdep_kgNha.rename(index = shapefile['CNTRY_NAME'])
-df_cropland_km2= pd.DataFrame(0, index=np.arange(len(shapefile)), columns=np.arange(1961,2019,1))
+df_cropland_km2= pd.DataFrame(0, index=np.arange(len(shapefile)), columns=np.arange(1961,2020,1))
 df_cropland_km2 = df_cropland_km2.rename(index = shapefile['CNTRY_NAME'])
 # NOx
-for yr in np.arange(1961,2019,1):
+for yr in np.arange(1961,2020,1):
     print(yr)
     if yr > 2015:
         yrcr = 2015
@@ -53,12 +53,12 @@ for co in range(0,len(shapefile['CNTRY_NAME'])):
     plt.clf()
     #plt.figure()
     plt.plot(df_totNdep_kgNha_trans[shapefile['CNTRY_NAME'][co]],color = 'red')
-    plt.xlim(1961,2018)
+    plt.xlim(1961,2020)
     plt.ylabel('Total N Deposition (kgN/ha)')
     plt.xlabel('Years')
     plt.title(shapefile['CNTRY_NAME'][co])
     plt.ylim([0,20])
-    plt.savefig(r".\Results\LUH2\Time_Series_Co_1961_2018\TotNdep_{0}_kgNha.png".format(shapefile['CNTRY_NAME'][co]), dpi=200)
+    plt.savefig(r".\Results\LUH2\Time_Series_Co_1961_2020\TotNdep_{0}_kgNha.png".format(shapefile['CNTRY_NAME'][co]), dpi=200)
 
 # to csv
 df_totNdep_kgNha.to_csv("df_totNdep_kgNha.csv")
@@ -71,7 +71,7 @@ df_cropland_km2.to_csv("df_cropland_km2.csv")
 df_totNdep_kgNha.loc['Taiwan'] = df_totNdep_kgNha.loc['China']
 ## plotting map: total N kgN/ha
 #df_ndep = pd.DataFrame({'Ndep_kgN':xx_kgN,'ISO3':shapefile['ISO3']})
-for yr_to_plot in np.arange(1961,2019,1):
+for yr_to_plot in np.arange(1961,2020,1):
     plt.clf()
     df_ndep_map_kgNha = pd.DataFrame({'Ndep_kgNha':np.array(df_totNdep_kgNha[yr_to_plot]),'CNTRY_NAME':shapefile['CNTRY_NAME']})
 
@@ -84,47 +84,5 @@ for yr_to_plot in np.arange(1961,2019,1):
                               ,vmax = 30, vmin = 0  )
     ax.set_title("N deposition (kgN/ha) {0} ".format(yr_to_plot))
     #(missing values \nare plotted in grey)
-    plt.savefig(r".\Results\ACCMIP\LUH2\Yearly_maps_1961_2018_SameLim\Ndep_{0}_kgNha_m.png".format(yr_to_plot), dpi=200)
-
-
-# nox
-df_NOx_kgNha.loc['Taiwan'] = df_NOx_kgNha.loc['China']
-## plotting map: total N kgN/ha
-#df_ndep = pd.DataFrame({'Ndep_kgN':xx_kgN,'ISO3':shapefile['ISO3']})
-for yr_to_plot in np.arange(1961,2019,1):
-    plt.figure()
-    df_ndep_map_kgNha = pd.DataFrame({'NOx_kgNha':np.array(df_NOx_kgNha[yr_to_plot]),'CNTRY_NAME':shapefile['CNTRY_NAME']})
-
-    #df_new = df_ndep.fillna(0)
-    shapefile_m = shapefile.merge(df_ndep_map_kgNha,on='CNTRY_NAME')
-
-    # plotting the map
-    ax = shapefile_m.plot(color="grey")
-    shapefile_m.dropna().plot(ax = ax, column = "NOx_kgNha",legend = True, cmap ='jet', legend_kwds={'label': "NOx deposition (kgN/ha)",'orientation': "horizontal" }
-                              ,vmax = 10, vmin = 0  )
-    ax.set_title("NOx deposition (kgN/ha) {0} ".format(yr_to_plot))
-    #(missing values \nare plotted in grey)
-    plt.savefig(r".\Results\LUH2\Yearly_Maps_1961_2018\NOx\NOx_{0}_kgNha_m.png".format(yr_to_plot), dpi=200)
-    plt.close()
-
-# nox
-df_NHy_kgNha.loc['Taiwan'] = df_NHy_kgNha.loc['China']
-## plotting map: total N kgN/ha
-#df_ndep = pd.DataFrame({'Ndep_kgN':xx_kgN,'ISO3':shapefile['ISO3']})
-for yr_to_plot in np.arange(1961,2019,1):
-    plt.figure()
-    df_ndep_map_kgNha = pd.DataFrame({'NHy_kgNha':np.array(df_NHy_kgNha[yr_to_plot]),'CNTRY_NAME':shapefile['CNTRY_NAME']})
-
-    #df_new = df_ndep.fillna(0)
-    shapefile_m = shapefile.merge(df_ndep_map_kgNha,on='CNTRY_NAME')
-
-    # plotting the map
-    ax = shapefile_m.plot(color="grey")
-    shapefile_m.dropna().plot(ax = ax, column = "NHy_kgNha",legend = True, cmap ='jet', legend_kwds={'label': "NHy deposition (kgN/ha)",'orientation': "horizontal" }
-                              ,vmax = 10, vmin = 0  )
-    ax.set_title("NOx deposition (kgN/ha) {0} ".format(yr_to_plot))
-    #(missing values \nare plotted in grey)
-    plt.savefig(r".\Results\LUH2\Yearly_Maps_1961_2018\NHy\NHy_{0}_kgNha_m.png".format(yr_to_plot), dpi=200)
-    plt.close()
-
+    plt.savefig(r".\Results\ACCMIP\LUH2\Yearly_maps_1961_2020_SameLim\Ndep_{0}_kgNha_m.png".format(yr_to_plot), dpi=200)
 
